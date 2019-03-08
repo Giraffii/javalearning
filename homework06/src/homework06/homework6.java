bbe224ba6c4cfafa738cde72a61a73be81fdf40b
unico <unico@192.168.1.9>
2019-03-08 14:07:56 +0800

    homework6

--- homework06/src/homework06/homework6.java
+++ homework06/src/homework06/homework6.java
@@ -1,15 +1,15 @@
-package lesson06;
+package homework06;
 
 import javax.imageio.*;
 import java.awt.*;
 import java.awt.event.*;
-import javax.swing.*;
 import java.io.*;
+import javax.swing.*;
 
-
-public class prog6 {
+public class homework6 {
 
 	public static void main(String[] args) {
+		// TODO Автоматически созданная заглушка метода
 		
 		MainFrame window = new MainFrame ();
 	}
@@ -25,37 +25,52 @@ class MainPanel extends JPanel {
 		@Override
 		public void keyPressed (KeyEvent arg0) {
 			int code = arg0.getKeyCode();
-			if (code == 37) { direction = 0; } //LEFT
-			if (code == 38) { direction = 1; } //UP
-			if (code == 39) { direction = 2; } //RIGHT
-			if (code == 40) { direction = 3; } //DOWN
+			if (code == 37) { direction = 0; }
+			if (code == 38) { direction = 1; }
+			if (code == 39) { direction = 2; }
+			if (code == 40) { direction = 3; }
 		}
 		@Override
 		public void keyReleased (KeyEvent e) {}
 		@Override
-		public void keyTyped(KeyEvent e) {}
+		public void keyTyped (KeyEvent e) {}
 	}
 	
 	public MainPanel () {
 		x=0;
 		y=0;
-		addKeyListener(new MainKey());
-		setFocusable(true);
-		try {
-			image = ImageIO.read(new File ("src\\lesson06\\earth.jpg"));
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
+		addKeyListener (new MainKey());
+		setFocusable (true);
 		Timer timer = new Timer (100, new ActionListener() {
 			public void actionPerformed(ActionEvent event) {
-				if (direction == 0) x--;
-				if (direction == 1) y--;
-				if (direction == 2) x++;
-				if (direction == 3) y++;
+				if (direction == 0) {
+					x--;
+					try {
+						image = ImageIO.read(new File("\\Users\\unico\\Pictures\\LEFT.png"));
+					} catch (Exception e) {}
+				}
+				if (direction == 1) {
+					y--;
+					try {
+						image = ImageIO.read(new File("\\Users\\unico\\Pictures\\UP.png"));
+					} catch (Exception e) {}
+				}
+				if (direction == 2) {
+					x++;
+					try {
+						image = ImageIO.read(new File("\\Users\\unico\\Pictures\\RIGHT.png"));
+					} catch (Exception e) {}
+				}
+				if (direction == 3) {
+					y++;
+					try {
+						image = ImageIO.read(new File("\\Users\\unico\\Pictures\\DOWN.png"));
+					} catch (Exception e) {}
+				}
 				repaint();
 			}
 		});
-		timer.start();
+		timer.restart();	
 	}
 	public void paintComponent(Graphics graphics) {
 		graphics.clearRect(x-1, y-1, 301, 301);
